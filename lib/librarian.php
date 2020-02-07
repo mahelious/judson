1c2e5928a7cd8fcbf5c4ffa87fdb83e63ce9f0b0
Matthew Hill <mahelious@gmail.com>
2020-02-07 10:10:14 -0700

    fixes: - normalize tag pull, now compatible with FLAC - track the album artist from more than once source - abstract the primary tag for verification of tagging - strip slashes to prevent track names from corrupting filenames

--- lib/librarian.php
+++ lib/librarian.php
@@ -12,6 +12,15 @@ class Librarian
     private $source;
     private $destination;
 
+    const TAG_PRIMARY               = 'acoustid';
+    const COMMENT_TAG_ALBUMARTIST   = 'albumartist';
+    const COMMENT_TAG_ALBUMARTIST2  = 'band';
+    const COMMENT_TAG_ALBUMARTIST3  = 'band';
+    const COMMENT_TAG_ALBUM         = 'album';
+    const COMMENT_TAG_TRACK         = 'track_number';
+    const COMMENT_TAG_TITLE         = 'title';
+    const COMMENT_TAG_CATALOG       = 'catalognumber';
+
     /**
      * Initialize a new Librarian
      *
@@ -80,23 +89,46 @@ class Librarian
      */
     public function sort(string $file): string
     {
-        $file_info = $this->ID3->analyze($file);
+        try {
+            // getID3::analyze throws Errors, yay!
+            $file_info = $this->ID3->analyze($file);
+        } catch (Error $e) {
+            throw new ErrorException("Error sorting '$file': " . $e->getMessage());
+        }
 
-        // if the Acousti Id is not present assume track data is not valid and skip processing
-        if (empty($file_info['id3v2']['comments']['text']['Acoustid Id'])) {
-            throw new Exception("Could not identify Acousti Id for '$file'");
+        // normalize comments from all tag types
+        getid3_lib::CopyTagsToComments($file_info);
+
+        // if the primary tag is not found assume the tagging is absent or irregular
+        if (empty($file_info['comments']) || !$this->hasPrimaryTag($file_info['comments'])) {
+            throw new Exception("$file missing information tags, please rescan");
         }
 
         // collect the meta data used to generate new filepath
-        $artist = self::getFileComments(['band', 'artist'], $file_info);
-        $album = self::getFileComments(['album'], $file_info);
-        $track_number = self::getFileComments(['track_number'], $file_info);
-        $title = self::getFileComments(['title'], $file_info);
-        // prefer the current file extension to the id3vX extension; TODO - pull extension from file magic or id3vX ??
+        $artist = self::fileSanitize(self::getFileComments([
+            self::COMMENT_TAG_ALBUMARTIST,
+            self::COMMENT_TAG_ALBUMARTIST2,
+            self::COMMENT_TAG_ALBUMARTIST3,
+        ], $file_info['comments']));
+        $album  = self::fileSanitize(self::getFileComments([self::COMMENT_TAG_ALBUM], $file_info['comments']));
+        $catalog = self::fileSanitize(self::getFileComments([self::COMMENT_TAG_CATALOG], $file_info['comments']));
+        $track  = self::fileSanitize(self::getFileComments([self::COMMENT_TAG_TRACK], $file_info['comments']));
+        $title  = self::fileSanitize(self::getFileComments([self::COMMENT_TAG_TITLE], $file_info['comments']));
+        // prefer the current file extension over file magic or tagging; may reconsider this decision
         $extension = pathinfo($file, PATHINFO_EXTENSION);
 
+        // artist, album, track, and title are required to match
+        $required_fields = ['artist', 'album', 'track', 'title'];
+        foreach ($required_fields as $fieldname) {
+            if (empty($$fieldname)) {
+                throw new Exception("could not identify $fieldname");
+            }
+        }
+
         // save as the format "./ArtistName - AlbumName/TrackNumber - TrackName.ext"
-        return $this->moveFile($file, "$track_number - $title.$extension", "$artist - $album");
+        $foldername = "$artist - $album" . (!empty($catalog) ? " ($catalog)" : '');
+        $filename = "$track - $title.$extension";
+        return $this->moveFile($file, $filename, $foldername);
     }
 
     /**
@@ -118,7 +150,7 @@ class Librarian
 
         if (!copy($source, $destination)) {
             $err = error_get_last();
-            throw new Exception("cp $source failed, {$err['message']} ({$err['type']})");
+            throw new ErrorException("Error: {$err['message']}");
         }
 
         if (!unlink($source)) {
@@ -126,6 +158,40 @@ class Librarian
         }
 
         return $destination;
+    }
+
+    /**
+     * Determine whether the Primary tag is present in the file tags
+     *
+     * Primary tag is a keystone tag indicating that tags have been applied normally.
+     *
+     * @param array $comments
+     * @return bool
+     */
+    protected function hasPrimaryTag(array $comments): bool
+    {
+        foreach ($comments as $k => $v) {
+            if (is_array($v) && $this->hasPrimaryTag($comments[$k])) {
+                return true;
+            }
+            if (!empty($v) && strpos(strtolower($k), self::TAG_PRIMARY) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Remove slashes '/' & '\' that may interfere with filenames
+     *
+     * @param string $file_part
+     * @return string
+     */
+    public static function fileSanitize(string $file_part): string
+    {
+        // drop multiple whitespace that tends to occur when slicing slashes
+        return mb_ereg_replace('\s+', ' ', str_replace('\\', '', str_replace('/', '', $file_part)));
     }
 
     /**
@@ -160,20 +226,18 @@ class Librarian
     /**
      *
      * @param array $keys
-     * @param array $info
+     * @param array $comments
      * @return string
      * @throws Exception
      */
-    protected static function getFileComments(array $keys, array $info): string
+    protected static function getFileComments(array $keys, array $comments): string
     {
         foreach ($keys as $key) {
-            foreach (['id3v2', 'id3v1'] as $tag) {
-                if (!empty($info[$tag]['comments'][$key]) && count($info[$tag]['comments'][$key]) > 0) {
-                    return $info[$tag]['comments'][$key][0];
-                }
+            if (!empty($comments[$key]) && count($comments[$key]) > 0) {
+                return $comments[$key][0];
             }
         }
 
-        throw new Exception("could not identify $key");
+        return false;
     }
 }
